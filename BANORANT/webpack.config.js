a0f078fb20e30fac80433c6519b6354542bb7089
VinceCharles <vincecharles71@gmail.com>
2025-05-07 19:00:02 +0800

    commit changes.

--- BANORANT/webpack.config.js
+++ BANORANT/webpack.config.js
@@ -1,9 +1,23 @@
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+
+
+const envPath = path.resolve(__dirname, '.env');
+let env = {};
+if (fs.existsSync(envPath)) {
+  const dotenv = require('dotenv');
+  env = dotenv.config({ path: envPath }).parsed;
+}
 
 module.exports = {
   plugins: [
     new Dotenv({
-      systemvars: true, 
+      systemvars: true,
     }),
+    new webpack.DefinePlugin({
+      'process.env.VALORANT_API_KEY': JSON.stringify(env.VALORANT_API_KEY || process.env.VALORANT_API_KEY)
+    })
   ],
 };
