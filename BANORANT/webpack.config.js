386d77f06403fadec32d1c25b2c4fcb04ce613a1
VinceCharles <vincecharles71@gmail.com>
2025-05-15 18:53:58 +0800

    commit

--- BANORANT/webpack.config.js
+++ BANORANT/webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
 
-
 const envPath = path.resolve(__dirname, '.env');
 let env = {};
 if (fs.existsSync(envPath)) {
@@ -12,6 +11,25 @@ if (fs.existsSync(envPath)) {
 }
 
 module.exports = {
+  entry: './public/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new Dotenv({
       systemvars: true,
@@ -20,4 +38,7 @@ module.exports = {
       'process.env.VALORANT_API_KEY': JSON.stringify(env.VALORANT_API_KEY || process.env.VALORANT_API_KEY)
     })
   ],
+  resolve: {
+    extensions: ['.js']
+  }
 };
